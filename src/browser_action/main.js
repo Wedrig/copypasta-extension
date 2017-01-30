4079e76a25cfc2de542213a0ac184691c0bf07b9
Eric <Bax3n@users.noreply.github.com>
2017-01-29 18:13:20 -0600

    twitch spam

--- src/browser_action/main.js
+++ src/browser_action/main.js
@@ -3,19 +3,12 @@ copy = function(r) {
 }
 
 window.onclick = function(event) {
-    if (event.target == beemoviebutton) {
-        copy(beemovie);
-    }
-	if (event.target == shrek2button) {
-        copy(shrek2script);
-    }
-	if (event.target == shrek3button) {
-        copy(shrek3);
-    }
-	if (event.target == thelegend27button) {
-        copy(thelegend27);
-    }
-	if (event.target == ratatouillebutton) {
-        copy(rat);
-    }
+    if (event.target == beemoviebutton) {copy(beemovie);}
+	if (event.target == shrek2button) {copy(shrek2script);}
+	if (event.target == shrek3button) {copy(shrek3);}
+	if (event.target == thelegend27button) {copy(thelegend27);}
+	if (event.target == ratatouillebutton) {copy(rat);}
+	if (event.target == mariobutton) {copy(mario.innerHTML);}
+	if (event.target == ruskibutton) {copy(ruski.innerHTML);}
+	if (event.target == pepebutton) {copy(pepe.innerHTML);}
 }
